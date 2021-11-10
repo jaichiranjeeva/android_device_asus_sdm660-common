102f181dae8cc87b41fce077c6e10e1b0c7013fb
Sonal Singh <sonal.singh.19993@gmail.com>
2021-11-10 16:42:23 +0000

    sdm660-common: arrow => evolution
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.0 packages/apps/GCamGOPrebuilt
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
